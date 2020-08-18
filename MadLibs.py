e762a2a4e907095fcefd087f9b580612908c36a1
Ian Hodgetts <ian.hodgetts@capita.co.uk>
2020-08-18 19:37:37 +0100

    Initial app

---
+++ MadLibs.py
@@ -0,0 +1,12 @@
+from random import random
+
+print('Mad Libs Generator:')
+noun = input ('Enter noun:')
+verb = input ('Enter verb:')
+adjective = input ('Enter adjective:')
+adverb = input ('Enter adverb:')
+
+print('noun=', noun)
+print('verb=', verb)
+print('adjective=', adjective)
+print('adverb=', adverb)
