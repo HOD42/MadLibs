8278756efbfef3292a76cc8f6c7b3a71d41e21bb
Ian Hodgetts <ian.hodgetts@capita.co.uk>
2020-08-20 17:14:21 +0100

    Refresh now repo is public

--- MadLibs.py
+++ MadLibs.py
@@ -1,6 +1,12 @@
 from random import random
 
 print('Mad Libs Generator:')
+
+phrase_list = ['This is phrase one',
+'This is a second phrase',
+'Phrase number 3',
+'This is the last phrase']
+
 noun = input ('Enter noun:')
 verb = input ('Enter verb:')
 adjective = input ('Enter adjective:')
